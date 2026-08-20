38b1eac6c7931f3d3d32975539014111525908a6
Sujaygouda R Patil <sujaygoudarpatilweb3@gmail.com>
2026-08-20 22:42:18 +0530

    feat(react-full-course-sheriyans): create 2 sections, navbar, section1 content components.

---
+++ ReactJS-Full-Course-Sheriyans-coding-school/full-course/07-ui-project/src/components/Section1/Section1.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar"
+import Section1Content from "./Section1Content"
+
+const Section1 = () => {
+    return (
+        <div className='h-screen w-full '>
+            <Navbar />
+            <Section1Content />
+        </div>
+    )
+}
+
+export default Section1
---
+++ ReactJS-Full-Course-Sheriyans-coding-school/full-course/07-ui-project/src/components/Section1/Section1Content.jsx
@@ -0,0 +1,8 @@
+
+const Section1Content = () => {
+    return (
+        <div className="py-3"></div>
+    )
+}
+
+export default Section1Content
