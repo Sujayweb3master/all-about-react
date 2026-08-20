a15e2739cab3d15ec629ccadf47b94a2de39f49e
Sujaygouda R Patil <sujaygoudarpatilweb3@gmail.com>
2026-08-20 23:23:12 +0530

    feat(sheriyans-react-course): create components for section component

---
+++ ReactJS-Full-Course-Sheriyans-coding-school/full-course/07-ui-project/src/components/Section1/RightContent.jsx
@@ -0,0 +1,8 @@
+
+const RightContent = () => {
+    return (
+        <div className="h-full w-[72%] bg-blue-400"></div>
+    )
+}
+
+export default RightContent
--- ReactJS-Full-Course-Sheriyans-coding-school/full-course/07-ui-project/src/components/Section1/Section1Content.jsx
+++ ReactJS-Full-Course-Sheriyans-coding-school/full-course/07-ui-project/src/components/Section1/Section1Content.jsx
@@ -1,7 +1,12 @@
+import LeftContent from "./LeftContent"
+import RightContent from "./RightContent"
 
 const Section1Content = () => {
     return (
-        <div className="py-3"></div>
+        <div className="py-3 px-18 flex justify-between items-center h-[90vh] gap-4 bg-amber-800">
+            <LeftContent />
+            <RightContent />
+        </div>
     )
 }
 
