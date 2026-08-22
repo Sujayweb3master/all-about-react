843f7ff08dcf269f93335dcd3a38a01df35a7dfb
Sujaygouda R Patil <sujaygoudarpatilweb3@gmail.com>
2026-08-22 12:00:35 +0530

    feat(sheriyans-react-course): add 08-functions related code

---
+++ ReactJS-Full-Course-Sheriyans-coding-school/full-course/08-functions/src/App.jsx
@@ -0,0 +1,17 @@
+
+const App = () => {
+
+  const onchange = (event) => {
+
+    console.log('input is changed', event)
+  }
+
+
+  return (
+    <div>
+      <input type="text" onChange={onchange} placeholder="Enter name" />
+    </div>
+  )
+}
+
+export default App
